e3abca0a1ec8de278caa0f4f5e4b7e91272c9b1f
Manvadariya <manvadariya2@gmail.com>
2024-02-24 20:00:12 +0530

    importing student details from server and dispay them

--- face.py
+++ face.py
@@ -1,14 +1,24 @@
 # importing necessary libraries
-import pickle
-
-import cv2
 import os
-
+import pickle
+import cv2
 import cvzone
 import face_recognition
 import numpy as np
+from datetime import datetime
+# database
+import firebase_admin
+from firebase_admin import credentials
+from firebase_admin import db
+from firebase_admin import storage
 
+cred = credentials.Certificate("key.json")
+firebase_admin.initialize_app(cred, {
+    'databaseURL': 'https://faceattendance-c7b33-default-rtdb.firebaseio.com/',
+    'storageBucket': 'faceattendance-c7b33.appspot.com'
+})
 
+bucket = storage.bucket()
 # capture video from camera (from web cam)
 cap = cv2.VideoCapture(1)  # put 0 for default
 
@@ -47,7 +57,9 @@ print("Encode File loaded successfully")
 
 
 modeType = 0
-
+counter = 0
+id = -1
+imgStudent = []
 # capture video frame by frame, while q is pressed
 while True:
     ret, img = cap.read()
@@ -71,7 +83,6 @@ while True:
     imgBackground[44:44 + 633, 808:808 + 414] = imgModeList[modeType]
 
     if faceCurFrame:
-        # iterate encodeCurFrame and faceCurFrame from zip
         for encodeFace, faceLoc in zip(encodeCurFrame, faceCurFrame):
             matches = face_recognition.compare_faces(encodeListKnown, encodeFace)
             faceDis = face_recognition.face_distance(encodeListKnown, encodeFace)
@@ -80,14 +91,43 @@ while True:
 
             matchIndex = np.argmin(faceDis)
             # print(matchIndex)
-            # if find any face matches with draw border around it
+
             if matches[matchIndex]:
                 # print("Face Detected")
                 # print(studentIds[matchIndex])
                 y1, x2, y2, x1 = faceLoc
                 y1, x2, y2, x1 = y1 * 4, x2 * 4, y2 * 4, x1 * 4
-                bbox = (65 + x1, 210 + y1, x2 - x1, y2 - y1)
+                bbox = (55 + x1, 162 + y1, x2 - x1, y2 - y1)
                 cvzone.cornerRect(imgBackground, bbox, rt=0)
+                id = studentIds[matchIndex]
+                if counter == 0:
+                    counter = 1
+                    modeType = 1
+        if counter != 0:
+
+            if counter == 1:
+                # get the student Data
+                studentInfo = db.reference(f'Students/{id}').get()
+                print(studentInfo)
+                # Get the Image from the storage
+                blob = bucket.get_blob(f'Images/{id}.jpeg')
+                array = np.frombuffer(blob.download_as_string(), np.uint8)
+                imgStudent = cv2.imdecode(array, cv2.COLOR_BGRA2BGR)
+
+
+            cv2.putText(imgBackground, str(studentInfo['total_attendance']), (861,125), cv2.FONT_HERSHEY_COMPLEX, 1 ,(255,255,255), 1)
+            cv2.putText(imgBackground, str(studentInfo['branch']), (1026,552), cv2.FONT_HERSHEY_COMPLEX, 0.8 ,(255,255,255), 1)
+            cv2.putText(imgBackground, str(id), (1025,496), cv2.FONT_HERSHEY_COMPLEX, 0.8 ,(255,255,255), 1)
+            cv2.putText(imgBackground, str(studentInfo['class']), (910,625), cv2.FONT_HERSHEY_COMPLEX, 0.6 ,(100,100,100), 1)
+            cv2.putText(imgBackground, str(studentInfo['year']), (1025,625), cv2.FONT_HERSHEY_COMPLEX, 0.6 ,(100,100,100), 1)
+            cv2.putText(imgBackground, str(studentInfo['starting_year']), (1125,625), cv2.FONT_HERSHEY_COMPLEX, 0.6 ,(100,100,100), 1)
+            (w, h), _ = cv2.getTextSize(studentInfo['name'], cv2.FONT_HERSHEY_COMPLEX, 1, 1)
+            offset = (414 - w) // 2
+            cv2.putText(imgBackground, str(studentInfo['name']), (808 + offset, 445), cv2.FONT_HERSHEY_COMPLEX, 1, (50, 50, 50),
+                        1)
+
+            imgBackground[175:175 + 216, 909:909 + 216] = imgStudent
+            counter += 1
 
     cv2.imshow('frame', imgBackground)
     if cv2.waitKey(1) & 0xFF == ord('q'):
