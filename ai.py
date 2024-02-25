69e10d301c0156864c594c647132714ef8a8dec6
Manvadariya <manvadariya2@gmail.com>
2024-02-25 17:20:22 +0530

    adding ai and its feature

---
+++ ai.py
@@ -0,0 +1,22 @@
+# importing neccessary module
+
+import speech_recognition as sr #pip install speechrecognition
+# Listening commands
+def Listen():
+    r = sr.Recognizer()
+
+    with sr.Microphone() as source:
+        print("Listening...")
+        r.pause_threshold = 1
+        audio = r.listen(source, 0, 8)  # Listening Mode.....
+
+    try:
+        print("Recognizing...")
+        query = r.recognize_google(audio, language="en")
+
+    except:
+        return ""
+
+    query = str(query).lower()
+    print("You: " + query)
+    return query
