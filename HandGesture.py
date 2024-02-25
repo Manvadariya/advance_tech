c9c38af48da7d3bc112312e544e4ec5b17d682b8
Manvadariya <manvadariya2@gmail.com>
2024-02-25 11:37:42 +0530

    Add more fitures for stable volume controll

--- HandGesture.py
+++ HandGesture.py
@@ -13,7 +13,6 @@ cap = cv2.VideoCapture(1)  # Set your camera appropriately by changing count Ex:
 
 # defining a class for hand tracking
 class handDetector():
-    # initializing class
     def __init__(self, mode=False, maxHands=2, detectionCon=0.5, trackCon=0.5):
         self.mode = mode
         self.maxHands = maxHands
@@ -22,41 +21,72 @@ class handDetector():
 
         self.mpHands = mp.solutions.hands
         self.hands = self.mpHands.Hands(self.mode, self.maxHands, int(self.detectionCon), self.trackCon)
-
         self.mpDraw = mp.solutions.drawing_utils
+        self.tipIds = [4, 8, 12, 16, 20]
 
-    # find hand and draw landmarks
     def findHands(self, img, draw=True):
         imgRGB = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
         self.results = self.hands.process(imgRGB)
+
         if self.results.multi_hand_landmarks:
             for handLms in self.results.multi_hand_landmarks:
                 if draw:
-                    self.mpDraw.draw_landmarks(img, handLms,
-                                               self.mpHands.HAND_CONNECTIONS)
+                    self.mpDraw.draw_landmarks(img, handLms, self.mpHands.HAND_CONNECTIONS)
         return img
 
-    # finding position of hand
     def findPosition(self, img, handNo=0, draw=True):
         xList = []
         yList = []
-
+        bbox = []
         self.lmList = []
+
         if self.results.multi_hand_landmarks:
             myHand = self.results.multi_hand_landmarks[handNo]
             for id, lm in enumerate(myHand.landmark):
-                # print(id, lm)
                 h, w, c = img.shape
                 cx, cy = int(lm.x * w), int(lm.y * h)
                 xList.append(cx)
                 yList.append(cy)
                 self.lmList.append([id, cx, cy])
                 if draw:
-                    cv2.circle(img, (cx, cy), 15, (255, 0, 255), cv2.FILLED)
+                    cv2.circle(img, (cx, cy), 5, (255, 0, 255), cv2.FILLED)
             xmin, xmax = min(xList), max(xList)
             ymin, ymax = min(yList), max(yList)
+            bbox = xmin, ymin, xmax, ymax
 
-        return self.lmList
+            if draw:
+                cv2.rectangle(img, (bbox[0] - 20, bbox[1] - 20), (bbox[2] + 20, bbox[3] + 20), (0, 255, 0), 2)
+
+        return self.lmList, bbox
+
+    def fingersUp(self):
+        fingers = []
+        # Thumb
+        if self.lmList[self.tipIds[0]][1] > self.lmList[self.tipIds[0] - 1][1]:
+            fingers.append(1)
+        else:
+            fingers.append(0)
+        # 4 Fingers
+        for id in range(1, 5):
+            if self.lmList[self.tipIds[id]][2] < self.lmList[self.tipIds[id] - 2][2]:
+                fingers.append(1)
+            else:
+                fingers.append(0)
+        return fingers
+
+    def findDistance(self, p1, p2, img, draw=True):
+        x1, y1 = self.lmList[p1][1], self.lmList[p1][2]
+        x2, y2 = self.lmList[p2][1], self.lmList[p2][2]
+        cx, cy = (x1 + x2) // 2, (y1 + y2) // 2
+
+        if draw:
+            cv2.circle(img, (x1, y1), 15, (255, 0, 255), cv2.FILLED)
+            cv2.circle(img, (x2, y2), 15, (255, 0, 255), cv2.FILLED)
+            cv2.line(img, (x1, y1), (x2, y2), (255, 0, 255), 3)
+            cv2.circle(img, (cx, cy), 15, (255, 0, 255), cv2.FILLED)
+
+        length = math.hypot(x2 - x1, y2 - y1)
+        return length, img, [x1, y1, x2, y2, cx, cy]
 
 wCam, hCam = 640, 480
 
@@ -71,45 +101,50 @@ volume = cast(interface, POINTER(IAudioEndpointVolume))
 volRange = volume.GetVolumeRange()
 minVol = volRange[0]
 maxVol = volRange[1]
-vol= 0
-volPer=0
-
-cap = cv2.VideoCapture(1)
-cap.set(3, wCam)
-cap.set(4, hCam)
-
+vol = 0
+volPer = 0
+area = 0
+colorVol = (255, 0, 0)
 
 while True:
-    ret, img = cap.read()
+    success, img = cap.read()
 
     # Find Hand
     img = detector.findHands(img)
-    lmList = detector.findPosition(img, draw=False)
-
+    lmList, bbox = detector.findPosition(img, draw=True)
     if len(lmList) != 0:
-        # print(lmList[4], lmList[8])
-        x1,y1 = lmList[4][1], lmList[4][2]
-        x2,y2 = lmList[8][1], lmList[8][2]
-        cx,cy = (x1+x2)//2, (y1+y2)//2
-        cv2.circle(img, (x1,y1), 15, (255,0,255), cv2.FILLED)
-        cv2.circle(img, (x2,y2), 15, (255,0,255), cv2.FILLED)
-        cv2.line(img, (x1,y1), (x2,y2), (255,0,255), 3)
-        cv2.circle(img, (cx,cy), 15, (255,0,255), cv2.FILLED)
-        length = math.hypot(x2-x1, y2-y1)
-        print(length)
 
-        vol = np.interp(length, [50, 300], [minVol, maxVol])
-        volper = np.interp(length, [50, 300], [0, 100])
+        # Filter based on size
+        area = (bbox[2] - bbox[0]) * (bbox[3] - bbox[1]) // 100
+        # print(area)
+        if 250 < area < 1000:
 
-        print(int(length), vol)
-        volume.SetMasterVolumeLevel(vol, None)
+            # Find Distance between index and Thumb
+            length, img, lineInfo = detector.findDistance(4, 8, img)
+            # print(length)
 
+            # Convert Volume
+            volPer = np.interp(length, [50, 200], [0, 100])
 
-        if length < 50 :
-            cv2.circle(img, (cx, cy), 10, (0, 255, 0), cv2.FILLED)
+            # Reduce Resolution to make it smoother
+            smoothness = 5
+            volPer = smoothness * round(volPer / smoothness)
 
-    cv2.putText(img, f' : {int(volPer)} %', (40, 450), cv2.FONT_HERSHEY_COMPLEX, 1, (255, 0, 0), 2)
+            # Check fingers up
+            fingers = detector.fingersUp()
+            # print(fingers)x
 
+            # If pinky finger is down set volume
+            if not fingers[4]:
+                volume.SetMasterVolumeLevelScalar(volPer / 100, None)
+                cv2.circle(img, (lineInfo[4], lineInfo[5]), 15, (0, 255, 0), cv2.FILLED)
+                colorVol = (0, 255, 0)
+            else:
+                colorVol = (255, 0, 0)
+
+    # Drawings
+    cv2.putText(img, f'Vol Set: {int(volPer)}', (400, 50), cv2.FONT_HERSHEY_COMPLEX,
+                1, colorVol, 3)
     cv2.imshow("Detecting", img)
     if cv2.waitKey(1) & 0xFF == ord('q'):
         break
