5a3262c02231304ee3def09b163ed296e5f1c8cb
Manvadariya <manvadariya2@gmail.com>
2024-02-25 09:37:31 +0530

    successfully searching position of each landmark of hand

--- HandGesture.py
+++ HandGesture.py
@@ -23,21 +23,41 @@ class handDetector():
     def findHands(self, img, draw=True):
         imgRGB = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
         self.results = self.hands.process(imgRGB)
-
         if self.results.multi_hand_landmarks:
-            for hand_landmarks in self.results.multi_hand_landmarks:
+            for handLms in self.results.multi_hand_landmarks:
                 if draw:
-                    self.mpDraw.draw_landmarks(img, hand_landmarks, self.mpHands.HAND_CONNECTIONS)
-
+                    self.mpDraw.draw_landmarks(img, handLms,
+                                               self.mpHands.HAND_CONNECTIONS)
         return img
 
+    # finding position of hand
+    def findPosition(self, img, handNo=0, draw=True):
+        xList = []
+        yList = []
+
+        self.lmList = []
+        if self.results.multi_hand_landmarks:
+            myHand = self.results.multi_hand_landmarks[handNo]
+            for id, lm in enumerate(myHand.landmark):
+                # print(id, lm)
+                h, w, c = img.shape
+                cx, cy = int(lm.x * w), int(lm.y * h)
+                xList.append(cx)
+                yList.append(cy)
+                self.lmList.append([id, cx, cy])
+                if draw:
+                    cv2.circle(img, (cx, cy), 15, (255, 0, 255), cv2.FILLED)
+            xmin, xmax = min(xList), max(xList)
+            ymin, ymax = min(yList), max(yList)
+
+        return self.lmList
 
 while True:
     ret, img = cap.read()
 
     hand = handDetector()
     img = hand.findHands(img)
-
+    print(hand.findPosition(img))
     cv2.imshow("Detecting", img)
     if cv2.waitKey(1) & 0xFF == ord('q'):
         break
