e87bb8c2f55bc58120010ea2b55cce05f69496d2
Manvadariya <manvadariya2@gmail.com>
2024-02-25 16:36:48 +0530

    final frontend

--- main.py
+++ main.py
@@ -1,28 +1,44 @@
-import tkinter as tk
-from tkinter import PhotoImage
+from customtkinter import *
+from tkinter import *
+import subprocess
 
-def display_image():
-    image_path = "mainbackground.png"  # Replace with the actual path to your image
-    img = PhotoImage(file=image_path)
-    label = tk.Label(window, image=img)
-    label.image = img  # To prevent image from being garbage collected
-    label.pack()
+# wait for 20 seconds
 
-# Create the main Tkinter window
-window = tk.Tk()
-window.title("Youtube Video Downloader")
+def hand_program():
+    window.destroy()
+    subprocess.run(["python", r"HandGesture.py"], shell=True)
 
-# Set window size and position
-window_width = 1248
-window_height = 768
+def face_program():
+    window.destroy()
+    subprocess.run(["python", r"face.py"], shell=True)
+def ai_program():
+    window.destroy()
+    subprocess.run(["python", r"ai.py"], shell=True)
+
+window = CTk()
+window.title("ADVANCE TECHNOLOGY")
+
+# Set window dimensions and position
+window_width = 1000
+window_height = 610
 full_width = window.winfo_screenwidth()
 full_height = window.winfo_screenheight()
 place_width = int(full_width - full_width / 2 - window_width / 2)
-place_height = int(full_height - full_height / 2 - window_height / 2) - 40
+place_height = int(full_height - full_height / 2 - window_height / 2)
 window.geometry(f"{window_width}x{window_height}+{place_width}+{place_height}")
 
-# Call the function to display the image
-display_image()
+window.resizable(False, False)
 
-# Start the Tkinter event loop
+# Load and display an image
+image_path = "mainbackground.png"  # Replace with the actual path to your image
+img = PhotoImage(file=image_path)
+
+# Create a label to display the image
+image_label = Label(window, image=img)
+image_label.pack()
+
+CTkButton(master=window, text="GET START", width=120, height=30, fg_color="#ac74f9", hover_color="#dc85fc", command=face_program()).place(relx=0.179, rely=0.7)
+CTkButton(master=window, text="GET START", width=120, height=30, fg_color="#ac74f9", hover_color="#dc85fc", command=ai_program()).place(relx=0.705, rely=0.7)
+CTkButton(master=window, text="GET START", width=140, height=40, fg_color="#ac74f9", hover_color="#dc85fc", command=hand_program).place(relx=0.433, rely=0.73)
+
 window.mainloop()
