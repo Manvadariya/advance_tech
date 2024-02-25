21fe3ee96da8ff0d1bac7bee1ee5e3b82a04247b
Manvadariya <manvadariya2@gmail.com>
2024-02-25 15:28:47 +0530

    place windo at proper place

--- main.py
+++ main.py
@@ -4,16 +4,25 @@ from tkinter import PhotoImage
 def display_image():
     image_path = "mainbackground.png"  # Replace with the actual path to your image
     img = PhotoImage(file=image_path)
-    label = tk.Label(root, image=img)
+    label = tk.Label(window, image=img)
     label.image = img  # To prevent image from being garbage collected
     label.pack()
 
 # Create the main Tkinter window
-root = tk.Tk()
-root.title("ADVANCE TECHNOLOGY")
+window = tk.Tk()
+window.title("Youtube Video Downloader")
+
+# Set window size and position
+window_width = 1248
+window_height = 768
+full_width = window.winfo_screenwidth()
+full_height = window.winfo_screenheight()
+place_width = int(full_width - full_width / 2 - window_width / 2)
+place_height = int(full_height - full_height / 2 - window_height / 2) - 40
+window.geometry(f"{window_width}x{window_height}+{place_width}+{place_height}")
 
 # Call the function to display the image
 display_image()
 
 # Start the Tkinter event loop
-root.mainloop()
+window.mainloop()
